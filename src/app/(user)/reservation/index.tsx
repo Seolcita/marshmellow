1d7ec49151cafbb707554e0148de2ac9614d7700
Seolcita <seolcita@gmail.com>
2024-05-15 14:50:33 -0600

    Added Reservation Screen

---
+++ src/app/(user)/reservation/index.tsx
@@ -0,0 +1,7 @@
+import ReservationScreen from '../../../components/screen/Reservation/ReservationScreen';
+
+const MainIndex = () => {
+  return <ReservationScreen />;
+};
+
+export default MainIndex;
---
+++ src/components/screen/Reservation/ReservationScreen.tsx
@@ -0,0 +1,7 @@
+import { Text } from '../../Themed';
+
+const ReservationScreen = () => {
+  return <Text>Reservation Screen</Text>;
+};
+
+export default ReservationScreen;
