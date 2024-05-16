4145e7ef16662132345bb8c010d966a1d3a530f3
Seolcita <seolcita@gmail.com>
2024-05-15 22:11:46 -0600

    added Reservation Modal

---
+++ src/components/composite/reservation/ReservationModal.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const ModalTitle = styled.Text`
+  font-size: 23px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const InputContainer = styled.View`
+  width: 100%;
+  margin-top: 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
+`;
