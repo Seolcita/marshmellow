24fbda2b09f03dfcf6868aff3c187bd81dc79cba
Seolcita <seolcita@gmail.com>
2024-06-19 12:27:27 -0600

    Added Space token

---
+++ src/styles/tokens/space-token.ts
@@ -0,0 +1,27 @@
+export type Space = {
+  space0: string;
+  space1: string;
+  space2: string;
+  space3: string;
+  space4: string;
+  space5: string;
+  space6: string;
+  space7: string;
+  space8: string;
+  space9: string;
+  space10: string;
+};
+
+export const space: Space = {
+  space0: '0px',
+  space1: '4px',
+  space2: '8px',
+  space3: '12px',
+  space4: '16px',
+  space5: '20px',
+  space6: '24px',
+  space7: '32px',
+  space8: '40px',
+  space9: '48px',
+  space10: '56px',
+};
