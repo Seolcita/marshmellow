05ed8b48acaf8e70954a7163a32c8e0df2393fdd
Seolcita <seolcita@gmail.com>
2024-06-19 10:28:55 -0600

    UI updated

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -5,9 +5,13 @@ import { useAuth } from '../../providers/AuthProvider';
 export default function AuthLayout() {
   const { session } = useAuth();
 
-  if (session) {
-    return <Redirect href={'/(user)/main'} />;
-  }
+  // if (session) {
+  //   return <Redirect href={'/(user)/main'} />;
+  // }
 
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  );
 }
